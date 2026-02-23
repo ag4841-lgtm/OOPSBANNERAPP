25ccd144e9bcd40b886de1325f00f7094fd09aa6
ag4841-lgtm <ag4841@srmist.edu.in>
2026-02-23 10:59:02 +0530

    update code

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,34 +1,12 @@
 /**
- * OOPSBannerApp - UC2
- * Prints OOPS in banner format using * and spaces
- * @author YourName
- * @version 1.1
+ * OOPSBannerApp
+ * UC1: Print OOPS to Console
+ * @author Ananth
+ * @version 1.0
  */
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
-
-        // Line 1
-        System.out.println("  *****   " + "  *****   " + "  ******  " + "   *****  ");
-
-        // Line 2
-        System.out.println(" **   **  " + " **   **  " + " **   **  " + "  **      ");
-
-        // Line 3
-        System.out.println(" **   **  " + " **   **  " + " **   **  " + "   ***    ");
-
-        // Line 4
-        System.out.println(" **   **  " + " **   **  " + " ******   " + "      **  ");
-
-        // Line 5
-        System.out.println(" **   **  " + " **   **  " + " **       " + "      **  ");
-
-        // Line 6
-        System.out.println(" **   **  " + " **   **  " + " **       " + " **   **  ");
-
-        // Line 7
-        System.out.println("  *****   " + "  *****   " + " **        " + "  *****   ");
-
+        System.out.println("OOPS");
     }
 }
-
